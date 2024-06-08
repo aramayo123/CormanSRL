62d30230a5a8c5883db2432f695c560ded343d1f
aramayo123 <aramayo420@gmail.com>
2024-06-08 19:34:10 -0300

    cambio el redireccionamiento al registrar

--- routes/web.php
+++ routes/web.php
@@ -19,47 +19,6 @@ use App\Models\User;
 
 Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
-Route::get('/google-auth/redirect', function () {
-    return Socialite::driver('google')->redirect();
-});
-
-Route::get('/google-auth/callback', function () {
-    $user_obtenido = Socialite::driver('google')->stateless()->user();
-
-    try {
-        $user = User::updateOrCreate([
-            'google_id' => $user_obtenido->id,
-        ], [
-            'name' => $user_obtenido->name,
-            'email' => $user_obtenido->email
-        ]);
-    }catch (\Exception $error){
-        throw ValidationException::withMessages([
-            'email' => trans('Lo siento! No puedes registrarte ni logearte con este correo por que ya esta en uso'),
-        ]);
-    }
-    Auth::login($user);
-
-    return redirect('/');
-});
-
-Route::get('/facebook-auth/redirect', function () {
-    return Socialite::driver('facebook')->redirect();
-});
-
-Route::get('/facebook-auth/callback', function () {
-    $user_obtenido = Socialite::driver('facebook')->stateless()->user();
-    $user = User::updateOrCreate([
-        'facebook_id' => $user_obtenido->id,
-    ], [
-        'name' => $user_obtenido->name,
-        'email' => $user_obtenido->email
-    ]);
-    Auth::login($user);
-    return redirect('/');
-});
-
-
 Route::get('/', function () {
     return view('index');
 })->middleware(['auth'])->name('dashboard');
