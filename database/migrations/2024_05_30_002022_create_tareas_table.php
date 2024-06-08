369a91edfe32d41dc0f898b8daa2813a28175909
aramayo123 <aramayo420@gmail.com>
2024-06-08 19:27:01 -0300

    mi primer commit

---
+++ database/migrations/2024_05_30_002022_create_tareas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+			$table->unsignedBigInteger('id_ticket');
+            $table->foreign('id_ticket')->references('id')->on('ots')->onDelete('cascade');
+			$table->string("");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
